0a83c7378cfc57e09db15c699e1eb192a15df2ac
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2022-04-20 18:57:25 +0200

    add first organisation

--- projet_fil_rouge_java/src/projet_fil_rouge_java/Fil_rouge.java
+++ projet_fil_rouge_java/src/projet_fil_rouge_java/Fil_rouge.java
@@ -2,15 +2,33 @@ package projet_fil_rouge_java;
 
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+import javax.swing.JTabbedPane;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
-public class Fil_rouge {
 
+public class Fil_rouge extends JFrame implements ActionListener{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4862798308073302717L;
 	private JFrame frame;
-
+	private JTextField textField;
+	private JButton btnNewButtonTest = new JButton("Cliquez-moi");
+	private JPanel panel = new JPanel();
+	private JLabel lblNewLabel = new JLabel("Test");
+	
 	/**
 	 * Launch the application.
 	 */
@@ -36,14 +54,50 @@ public class Fil_rouge {
 	public Fil_rouge() {
 		initialize();
 	}
-
+	
+	
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(600,400);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		frame.getContentPane().add(tabbedPane, BorderLayout.CENTER);
+		
+		
+		tabbedPane.addTab("Product", null, panel, null);
+		
+		
+		panel.add(lblNewLabel);
+		
+		
+		panel.add(btnNewButtonTest);
+		btnNewButtonTest.addActionListener(this);
+		
+		textField = new JTextField();
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JPanel panel_1 = new JPanel();
+		tabbedPane.addTab("Fournisseur", null, panel_1, null);
+		
+		JPanel panel_2 = new JPanel();
+		tabbedPane.addTab("Commande", null, panel_2, null);
+		
+		JPanel panel_3 = new JPanel();
+		tabbedPane.addTab("Stock", null, panel_3, null);
+	}
+	/**
+	 * Listen Button
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("btn click");
+		
 	}
 
 }
